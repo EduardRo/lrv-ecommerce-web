62f65c69a43573c5af73c93f134f4a32ddce26b5
EduardRo <eduardstancu@yahoo.com>
2023-10-01 11:20:33 +0300

    modif S45 187

---
+++ routes/vendor.php
@@ -0,0 +1,10 @@
+<?php
+
+use App\Http\Controllers\Backend\VendorController;
+use Illuminate\Support\Facades\Route;
+
+/** Vendor Routes */
+
+
+
+Route::get('vendor/dashboard', [VendorController::class, 'dashboard'])->middleware('auth', 'role:vendor')->name('vendor.dashboard');
